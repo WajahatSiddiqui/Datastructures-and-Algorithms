2dbc9724c6540b0de3a3ff8cca0d7986c6ef1b83
WajahatSiddiqui <wajahatmeister@gmail.com>
2015-08-09 19:21:08 +0530

    Implement O(n) algorithm for finding two numbers who matches given sum
    Signed-off-by: WajahatSiddiqui <wajahatmeister@gmail.com>

--- DataStructures/Arrays/find-two-numbers-whose-sum-is-x.cpp
+++ DataStructures/Arrays/find-two-numbers-whose-sum-is-x.cpp
@@ -23,6 +23,36 @@ void findTwoElements(int A[], int size, int x) {
         cout<<"Elements not found!"<<endl;
 }
 
+/**
+ * 1) Initialize two indexes which are candidates in the array
+ *  l = 0, r = size-1; // since size = array size
+ * 2) loop while l < r
+ *  sum = A[l] + A[r]
+ *  (i) if sum == x print A[l], A[r]
+ *  (ii) if sum < x r--
+ *  (iii) if sum > x l++
+ *
+ * T(n) = O(n)
+ */
+void findTwoElementsEfficiently(int A[], int size, int x) {
+    int l = 0, r = size-1, sum = 0;
+    bool found = false;
+    while (l < r) { 
+        sum = A[l] + A[r];
+        if (sum == x) {
+           if (A[r] < 0)
+                cout<<"Two Elements Are: "<<A[l]<<A[r]<<"="<<x<<endl;
+           else
+                cout<<"Two Elements Are: "<<A[l]<<"+"<<A[r]<<"="<<x<<endl;
+            found = true;
+            break;
+        } else if (sum < x) r--;
+        else l++;
+    }
+    if (!found)
+        cout<<"Elements not found!"<<endl;
+}
+
 void printArray(int A[], int size) {
     for (int i = 0; i < size; i++) {
         cout<<A[i]<<" ";
@@ -37,5 +67,6 @@ int main(int argc, char *argv[]) {
 
     printArray(A, size);
     findTwoElements(A, size, atoi(argv[1]));
+    findTwoElementsEfficiently(A, size, atoi(argv[1]));
     return 0;
 }
