08dbe2c76ffd11c24360d021b8d44c62b0c4abe1
WajahatSiddiqui <wajahatmeister@gmail.com>
2015-12-23 18:32:09 +0530

    Implement ConcurrentLRUCache
    Signed-off-by: WajahatSiddiqui <wajahatmeister@gmail.com>

---
+++ Java/TestConcurrentLRUCache.java
@@ -0,0 +1,17 @@
+
+public class TestConcurrentLRUCache {
+	
+	public static void main(String args[]) {
+		ConcurrentLRUCache<Integer, String> cache = new ConcurrentLRUCache<>(4);
+		cache.put(1, "a");
+		cache.put(2, "b");
+		cache.put(3, "c");
+		cache.put(1, "a");
+		cache.put(4, "d");
+		cache.put(5, "e");
+		
+		cache.printQueue();
+
+	}
+
+}
