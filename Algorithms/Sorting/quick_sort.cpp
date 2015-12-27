b360dd354707ff189fcc970f43e9897753cf4be3
WajahatSiddiqui <wajahatmeister@gmail.com>
2015-12-27 13:42:10 +0530

    Optimizing quick sort
    seleting the last element as pivot
    
    Signed-off-by: WajahatSiddiqui <wajahatmeister@gmail.com>

--- Algorithms/Sorting/quick_sort.cpp
+++ Algorithms/Sorting/quick_sort.cpp
@@ -6,7 +6,6 @@ using namespace std;
 // Worst case: O(n^2)
 void quick_sort(int A[], int low, int high);
 int  partition(int A[], int low, int high);
-void swap(int A[], int &low, int &high);
 
 int main() {
 	int A[] = {5, 6, 8, 1, -1, 0, 11, 100, -5, 0, 1, 0, 6, 1, 2, 3, 5, 10000};
@@ -25,34 +24,34 @@ int main() {
 	return 0;
 }
 
+void swap(int *n1, int *n2) {
+    int tmp = *n1;
+    *n1 = *n2;
+    *n2 = tmp;
+}
+
 // low represents starting index
 // high represents size-1, ending index
 void quick_sort(int A[], int low, int high) {
-    int pivot;
-    if (low >= high) return;
-    // Partition the array based on some pivot
-    pivot = partition(A, low, high);
-    // Decompose and sort recursively
-    quick_sort(A, low, pivot-1);
-    quick_sort(A, pivot+1, high);
+    if (low < high) {
+        // Partition the array based on some pivot
+        int pivot = partition(A, low, high);
+        // Decompose and sort recursively
+        quick_sort(A, low, pivot-1);
+        quick_sort(A, pivot+1, high);
+    }
 }
 
 int partition(int A[], int low, int high) {
-    int pivot = A[low];
-    int left = low;
-    int right = high;
-    while (left < right) {
-        while (A[left] <= pivot)
-            left++;
-        while (A[right] > pivot)
-            right--;
-        if (left < right) {
-            int tmp = A[left];
-            A[left] = A[right];
-            A[right] = tmp;
+    int pivot = A[high];
+    int  i = low - 1;
+    for (int j = low; j <= high-1; j++) {
+        if (A[j] <= pivot) {
+            i++;
+            swap(&A[i], &A[j]);
         }
     }
-    A[low] = A[right];
-    A[right] = pivot;
-    return right;
+    swap(&A[i+1], &A[high]);
+    return i+1;
 }
+
