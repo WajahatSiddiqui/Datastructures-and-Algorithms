03420805938a9f8955d2d12bc18ac0e90ac0878f
WajahatSiddiqui <wajahatmeister@gmail.com>
2016-03-23 23:29:29 +0530

    Find the max area under the histogram
    Signed-off-by: WajahatSiddiqui <wajahatmeister@gmail.com>

---
+++ DataStructures/Stacks/max_area_histogram.cpp
@@ -0,0 +1,43 @@
+// Program to evaluate the maximum area under the given histogram
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int getMaxArea(int A[], int N) {
+	stack<int> s;
+	int max = -1, top_area = 0, top = 0, i = 0;
+	while (i < N) {
+		if (s.empty() || A[s.top()] <= A[i])
+			s.push(i++);
+		else {
+			top = s.top();
+			s.pop();
+			if (s.empty()) {
+				top_area = A[top] * i;
+			} else {
+				top_area = A[top] * (i - s.top() - 1);
+			}
+			if (top_area > max) max = top_area;
+		}
+	}
+	while (!s.empty()) {
+			top = s.top();
+			s.pop();
+			if (s.empty()) {
+				top_area = A[top] * i;
+			} else {
+				top_area = A[top] * (i - s.top() - 1);
+			}
+			if (top_area > max) max = top_area;
+
+	}
+	return max;
+}
+
+int main() {
+	int A[] = {2, 1, 2};
+	int size = sizeof(A)/sizeof(A[0]);
+	cout<<getMaxArea(A, size)<<endl;
+
+	return 0;
+}
