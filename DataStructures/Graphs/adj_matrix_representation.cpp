593d6e5360bba91fca5e62b32dea33ee86d921f8
WajahatSiddiqui <wajahatmeister@gmail.com>
2016-04-08 17:18:40 +0530

    Implement DFS and BFS using adj matrix
    Signed-off-by: WajahatSiddiqui <wajahatmeister@gmail.com>

--- DataStructures/Graphs/adj_matrix_representation.cpp
+++ DataStructures/Graphs/adj_matrix_representation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <queue>
 using namespace std;
 
 struct Graph {
@@ -32,7 +33,7 @@ Graph* createGraph(int V, int E) {
         G->adj[u][v] = 1;
         // uncomment this for undirected graph
         // uses symmetry to initialize
-        //G->adj[v][u] = 1;
+        G->adj[v][u] = 1;
     }
     return G;
 }
@@ -46,6 +47,53 @@ void print(Graph *G) {
     }
 }
 
+void dfsUtil(Graph *G, int u, bool *visited) {
+    visited[u] = true;
+    cout<<u<<" ";
+    for (int i = 0; i < G->V; i++) {
+        if (G->adj[u][i] && !visited[i]) {
+            dfsUtil(G, i, visited);
+        }
+    }
+}
+
+void dfs(Graph *G) {
+    bool *visited = new bool[G->V];
+    for (int i = 0; i < G->V; i++) {
+        if (!visited[i])
+            dfsUtil(G, i, visited);
+    }
+    delete [] visited;
+}
+
+void bfsUtil(Graph *G, int u, bool *visited) {
+    visited[u] = true;
+    queue<int> q;
+    q.push(u);
+
+    while (!q.empty()) {
+        int v = q.front();
+        cout<<v<<" ";
+        q.pop();
+
+        for (int i = 0; i < G->V; i++) {
+            if (G->adj[i][u] && !visited[i]) {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+
+void bfs(Graph *G) {
+bool *visited = new bool[G->V];
+    for (int i = 0; i < G->V; i++) {
+        if (!visited[i])
+            bfsUtil(G, i, visited);
+    }
+    delete [] visited;   
+}
+
 void deleteGraph(Graph *G) {
     if (!G || !G->adj) return;
     for (int i = 0; i < G->V; i++)
@@ -56,8 +104,18 @@ void deleteGraph(Graph *G) {
 }
 
 int main() {
-    Graph *G = createGraph(4, 5);
+    int V, E;
+    cout<<"Enter #Vertices: ";
+    cin>>V;
+    cout<<"Enter #Edges: ";
+    cin>>E;
+    Graph *G = createGraph(4, 4);
+    cout<<"Printing Adjacency Matrix\n";
     print(G);
+    cout<<"DFS Traversal: ";
+    dfs(G);
+    cout<<"\nBFS Traversal: ";
+    bfs(G);
     deleteGraph(G);
     return 0;
 }
