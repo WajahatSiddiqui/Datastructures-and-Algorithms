98b799426a3f252700de102c59be25a9bdc4b2b9
WajahatSiddiqui <wajahatmeister@gmail.com>
2016-05-25 21:42:58 +0530

    Find Pythagorean Triplets in O(n^2Logn) time !

--- DataStructures/Arrays/pythagorean-triplets.cpp
+++ DataStructures/Arrays/pythagorean-triplets.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 //#define SQRT(X) (X)*(X)
 
@@ -6,24 +7,56 @@ int SQRT(int i) {
     return i*i;
 }
 
+/**
+ * Find the squares and check if square of hypotenuse
+ * is equal square of two sides
+ * T(N) = O(N^3)
+ **/
 void findTriplets(int A[], int size) {
+    int a, b, c;
     for (int i = 0; i < size; i++) {
         for (int j = i+1; j < size; j++) {
             for (int k = j+1; k < size; k++) {
-                if (SQRT(i) + SQRT(j) == SQRT(k)) {
-                    cout<<"Triplets: "<<i<<" "<<j<<" "<<k<<endl;
-                    return;
+                a = SQRT(A[i]); b = SQRT(A[j]); c = SQRT(A[k]);
+                //cout<<a<<" "<<b<<" "<<c<<endl;
+                if (a == b + c || b == a + c || c == a + b) {
+                    cout<<"Triplets: "<<A[i]<<" "<<A[j]<<" "<<A[k]<<endl;
+                    break;
                 }
             }
         }
     }
 }
 
+/**
+ * T(N) = O(N^2logN)
+ **/
+void findTripletsEfficient(int A[], int size) {
+    for (int i = 0; i < size; i++)
+        A[i] = A[i]*A[i];
+    sort(A, A+size);
+
+    for (int i = size-1; i >= 0; i--) {
+        int l = 0;
+        int r = size-1;
+        while (l < r) {
+            if (A[l] + A[r] == A[i]) {
+                cout<<"Triplets (E): "<<A[l]<<" "<<A[r]<<" "<<A[i]<<endl;
+                break;
+            } else {
+                if (A[l] + A[r] < A[i]) l++;
+                else r--;
+            }
+        }
+    }
+}
+
 int main() {
-    int A[] = {11, 12, 3, 4, 5};
+    int A[] = {11, 12, 4, 3, 5, 12, 13, 160, 231, 281};
     int size = sizeof(A)/sizeof(A[0]);
 
     findTriplets(A, size);
+    findTripletsEfficient(A, size);
 
     return 0;
 }
